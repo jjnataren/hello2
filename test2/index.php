0ccf009353436fa7231483d96046673e56d36ebb
Jesus Nataren <jesus.nataren@gmail.com>
2015-07-20 14:31:34 -0500

    test

--- test2/index.php
+++ test2/index.php
@@ -3,12 +3,6 @@ echo "test";
 
 echo "again tesxtyy";
 
-<<<<<<< HEAD
+
 
 echo "Mora changes here";
-=======
-echo "mast se";
-
-
-otro
->>>>>>> branch 'master' of https://github.com/jjnataren/hello2.git
